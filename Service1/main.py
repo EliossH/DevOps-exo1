12023cc76c8e267097606dc1a260768b66306454
EliossH <coheleachdamien@gmail.com>
2025-09-29 23:08:16 +0300

    Service1 can retrieve storage logs

--- Service1/main.py
+++ Service1/main.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from time import time
 from os import statvfs
+import requests
 
 start = time()
 app = Flask(__name__)
 
-@app.route("/status")
+@app.route("/status", methods=["GET"])
 def status():
     now = time()
     uptime = (now - start) / 3600
@@ -13,10 +14,10 @@ def status():
     space = storageStat.f_bfree * storageStat.f_frsize / 1000000
     return f"Timestamp1: uptime {uptime:.2f} hours, free disk in root: {round(space)} MBytes"
 
-@app.route("/log")
+@app.route("/log", methods=["GET"])
 def log():
-    print("log called")
-    return "log"
+    response = requests.get("http://storage:8080/log")
+    return response.text
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8070, debug=True)
