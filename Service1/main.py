9f0818f733a5b2b155e0fe1d40aad24fbe164e72
EliossH <coheleachdamien@gmail.com>
2025-09-29 23:19:38 +0300

    Service1 is now a proper proxy

--- Service1/main.py
+++ Service1/main.py
@@ -8,16 +8,30 @@ app = Flask(__name__)
 
 @app.route("/status", methods=["GET"])
 def status():
+    status_str = both_status()
+    response = requests.post("http://storage:8080/log", data=status_str)
+    return status_str + "\n"
+
+@app.route("/log", methods=["GET"])
+def log():
+    response = requests.get("http://storage:8080/log")
+    return response.text
+
+def own_status():
     now = time()
     uptime = (now - start) / 3600
     storageStat = statvfs("/")
     space = storageStat.f_bfree * storageStat.f_frsize / 1000000
     return f"Timestamp1: uptime {uptime:.2f} hours, free disk in root: {round(space)} MBytes"
 
-@app.route("/log", methods=["GET"])
-def log():
-    response = requests.get("http://storage:8080/log")
+def other_service_status():
+    response = requests.get("http://service2:8090/status")
     return response.text
+
+def both_status():
+    own = own_status()
+    other = other_service_status()
+    return f"{own}\n{other}"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8070, debug=True)
