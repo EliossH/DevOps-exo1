51774465ad53116164d9fb928fe671f1e42b5fb3
EliossH <coheleachdamien@gmail.com>
2025-09-29 22:57:54 +0300

    service1 : status is correct

--- Service1/main.py
+++ Service1/main.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from time import time
+from os import statvfs
 
+start = time()
 app = Flask(__name__)
 
 @app.route("/status")
 def status():
-    print("status called")
-    return "Status"
+    now = time()
+    uptime = (now - start) / 3600
+    storageStat = statvfs("/")
+    space = storageStat.f_bfree * storageStat.f_frsize / 1000000
+    return f"Timestamp1: uptime {uptime:.2f} hours, free disk in root: {round(space)} MBytes"
 
 @app.route("/log")
 def log():
